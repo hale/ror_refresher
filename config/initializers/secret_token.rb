5102aebfcec8949a9f6869c3c7dbcc145328e0d9
U-Philip-PC\Philip <Philip@Philip-PC.(none)>
2012-01-30 12:26:55 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorRefresher::Application.config.secret_token = '9e857d86a2f435e7b73db727a7f77f354e169896c82a0a344c7a1efe3d509fe423c0e9f1cd18f5af68e8321660ccf387efeba4a2310d731badbedebf75f7b90d'
